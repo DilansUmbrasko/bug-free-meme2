ec21a95cd7d0174837e3a8cf96b3210e668578ea
DilansUmbrasko <144351564+DilansUmbrasko@users.noreply.github.com>
2023-12-10 15:20:18 +0200

    Update dip225_dilans_umbrasko_2LD.py

--- 2LD/dip225_dilans_umbrasko_2LD.py
+++ 2LD/dip225_dilans_umbrasko_2LD.py
@@ -1,20 +1,26 @@
-import pandas as pd
+import pandas
+get_info=input()
 
-get_info = input()
 
-fails = pd.read_excel('2LD/description1.xlsx', sheet_name='LookupAREA')
+fails = pandas.read_excel('2LD/description1.xlsx' , sheet_name='LookupAREA')
+info_list = fails.values.tolist()
 
-filtered_df = fails[fails[1] == get_info]
+for x in range(len(info_list)):
+    i=info_list[x][1]
+    if i==get_info:
+        reg_name=info_list[x][0]
+        break
 
-reg_name = filtered_df.iloc[0, 0]
-
-region = []
+region=[]
 
 with open('2LD/data.csv', 'r') as f:
     next(f)
     for line in f:
-        row = line.rstrip().split(',')
-        if row[1] == reg_name:
-            region.append(int(row[3]))
+        row=line.rstrip().split(',')
+        try:
+            if row[1]==reg_name:
+                region.append(int(row[3]))
+        except NameError:
+            break
 
 print(sum(region))
